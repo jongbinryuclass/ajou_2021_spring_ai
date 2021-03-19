363358532c501f24b8de3d617e27399be5e70a22
hankyul2 <consistant1y@ajou.ac.kr>
2021-03-19 14:53:45 +0900

    Add Log

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -5,7 +5,7 @@ from Cryptodome.Util.Padding import pad
 
 def read_txt(fileName):
     with open(fileName, 'rt') as f:
-        list_data = list(f.readlines())
+        list_data = [a.strip('\n' for a in list(f.readlines())]
     return list_data
 
 def write_json(fileName, data):
@@ -29,12 +29,14 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 if __name__=="__main__":
     # 1.이메일을 통해서 전달 받은 키 파일의 경로 입력
-    key_path = "hankyul.pem"
+    key_path = "key.pem"
     # 2. 예측한 결과를 텍스트 파일로 저장했을 경우 리스트로 다시 불러오기
     # 본인이 원하는 방식으로 리스트 형태로 예측 값을 불러오기만 하면 됨(순서를 지킬것)
-    raw_ans_path = "ans.txt"
-    ans = read_txt(raw_ans_path)
-    # 3. 암호화된 파일을 저장할 위치
-    encrypt_ans_path = "../submission/hankyul/ans.json"
-    # 4. 암호화!(pycrytodome 설치)
-    encrypt_data(key_path, ans, encrypt_ans_path)
+    # ans2.txt 처럼
+    raw_ans_path = ['ans1.txt', 'ans2.txt'] # 본인의 예측 결과
+    encrypted_ans_path = ['ans1.json', 'ans2.json'] # 제출 파일 이름(이름 바꾸지 말것!)
+    type_cast = [int, str]
+    for raw_path, encrypted_path, cast in zip(raw_ans_path, encrypted_ans_path, type_cast):
+        ans = read_txt(raw_path)
+        # 3. 암호화!(pycrytodome 설치)
+        encrypt_data(key_path, ans, encrypted_path, cast)
