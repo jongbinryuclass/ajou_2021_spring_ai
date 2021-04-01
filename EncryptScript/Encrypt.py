70ba19596ace36fc002192747cc5f0f62fad89e3
hankyul2 <consistant1y@ajou.ac.kr>
2021-04-01 21:45:41 +0900

    Submit Professor

--- EncryptScript/Encrypt.py
+++ EncryptScript/Encrypt.py
@@ -29,12 +29,12 @@ def encrypt_data(key_path, ans_list, encrypt_store_path='ans.json'):
 
 if __name__=="__main__":
     # 1.이메일을 통해서 전달 받은 키 파일의 경로 입력
-    key_path = "key.pem"
+    key_path = "professor.pem"
     # 2. 예측한 결과를 텍스트 파일로 저장했을 경우 리스트로 다시 불러오기
     # 본인이 원하는 방식으로 리스트 형태로 예측 값을 불러오기만 하면 됨(순서를 지킬것)
-    raw_ans_path = "ans.txt"
+    raw_ans_path = "ans1.txt"
     ans = read_txt(raw_ans_path)
     # 3. 암호화된 파일을 저장할 위치
-    encrypt_ans_path = "../submission/team1/ans.json"
+    encrypt_ans_path = "../submission/professor/ans.json"
     # 4. 암호화!(pycrytodome 설치)
     encrypt_data(key_path, ans, encrypt_ans_path)
